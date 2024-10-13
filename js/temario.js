d86f6a273c3e72f19658569b2539e77acbfa49ec
Daniel <daniel-sevillano@hotmail.com>
2024-10-13 21:19:05 +0200

    Nueva calculadora de probabilidad.

---
+++ js/temario.js
@@ -0,0 +1,23 @@
+const formulario = document.querySelector("form");
+const temas = document.querySelector("#temas");
+
+function probabilidad(k) {
+    return k * (139 - k) * (k ** 2 - 139 * k + 9658) / 23319240 * 100;
+}
+
+function procesar(event) {
+    event.preventDefault();
+
+    const k = parseInt(temas.value);
+
+    let texto = document.querySelector("#texto");
+    if (!texto) {
+        texto = document.createElement("p");
+        texto.id = "texto";
+        formulario.append(texto);
+    }
+
+    texto.innerHTML = "La probabilidad de que salga al menos un tema de los que te has preparado es del <b>" + probabilidad(k).toFixed(2).replace(".", ",") + "%</b>.";
+}
+
+formulario.addEventListener("submit", procesar);
