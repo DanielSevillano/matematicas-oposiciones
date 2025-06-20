79c07c333231f67c668fa9812a03065be33464ba
Daniel <daniel-sevillano@hotmail.com>
2025-06-20 09:53:34 +0200

    Añadiendo una gráfica a la calculadora de probabilidad.

--- js/calculadora-probabilidad.js
+++ js/calculadora-probabilidad.js
@@ -1,5 +1,10 @@
+import { CategoryScale, Chart, Colors, Legend, LinearScale, LineController, LineElement, PointElement, Tooltip } from "https://cdn.jsdelivr.net/npm/chart.js@4/dist/chart.js/+esm";
+
+Chart.register(LineController, LineElement, PointElement, CategoryScale, LinearScale, Tooltip, Legend, Colors);
+
 const formulario = document.querySelector("form");
 const temas = document.querySelector("#temas");
+const grafica = document.querySelector("#grafica");
 
 function formatear() {
     if (math) return window.MathJax.typesetPromise();
@@ -25,5 +30,54 @@ function procesar(event) {
     texto.innerHTML = "La probabilidad de que salga al menos un tema de los que te has preparado es del <b>" + String(Math.floor(probabilidad(k) * 100) / 100).replace(".", ",") + "%</b>.";
 }
 
+async function crearGrafica() {
+    const ejeX = Array.from(Array(72).keys());
+    const ejeY = ejeX.map((k) => probabilidad(k));
+
+    const options = {
+        maintainAspectRatio: false,
+        plugins: {
+            legend: {
+                labels: {
+                    color: getComputedStyle(document.body).getPropertyValue("--on-button")
+                }
+            }
+        },
+        scales: {
+            x: {
+                grid: {
+                    color: getComputedStyle(document.body).getPropertyValue("--border-color")
+                },
+                ticks: {
+                    color: getComputedStyle(document.body).getPropertyValue("--on-button")
+                }
+            },
+            y: {
+                grid: {
+                    color: getComputedStyle(document.body).getPropertyValue("--border-color")
+                },
+                ticks: {
+                    color: getComputedStyle(document.body).getPropertyValue("--on-button")
+                }
+            }
+        }
+    };
+
+    new Chart(grafica, {
+        type: "line",
+        data: {
+            labels: ejeX,
+            datasets: [
+                {
+                    label: "Probabilidad",
+                    data: ejeY
+                }
+            ]
+        },
+        options: options
+    });
+}
+
 formatear();
 formulario.addEventListener("submit", procesar);
+crearGrafica();
